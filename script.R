10b9f98b50caa458d603be12bd17da921bb9fa0b
andyfeng13 <andy.h.feng.24@dartmouth.edu>
2022-05-19 00:47:37 -0400

    updated script

--- script.R
+++ script.R
@@ -2,16 +2,31 @@ library(tidyverse)
 library(lubridate)
 library(googlesheets4)
 
-file <- read_sheet("https://docs.google.com/spreadsheets/d/16i38oonuX1y1g7C_UAmiK9GkY7cS-64DfiDMNiR41LM/edit#gid=0")
+df <- read_sheet("https://docs.google.com/spreadsheets/d/16i38oonuX1y1g7C_UAmiK9GkY7cS-64DfiDMNiR41LM/edit#gid=0")
 
-
+## initial calculation
 df %>% summarise(mean(order_amount))
 
-df <- file %>% 
-  mutate(aov = order_amount / total_items)
 
-df %>% summarise(mean(aov))
+## corrected calculation
+df %>% summarise(aov = sum(order_amount) / sum(total_items))
 
-df %>% summarise(aov = sum(order_amount) / sum(total_items),
-                 total_items, order_amount)
+## shop 42 is an outlier
+df %>% group_by(shop_id) %>% 
+  summarise(n = mean(total_items)) %>%
+  arrange(-n)
+
+## shop 78 is also an outlier
+df %>% group_by(shop_id) %>% 
+  summarise(ave = mean(order_amount/total_items)) %>% 
+  arrange(-ave)
+
+## further correction without outliers
+df %>% 
+  filter(shop_id != 78, shop_id != 42) %>%
+  summarise(aov_mean = sum(order_amount) / sum(total_items),
+            aov_median = median(order_amount/total_items))
+
+
+
 
